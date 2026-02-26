7562ae1c21c8cd0fcc1449ab13ec9987ad1084c4
kapritomi <kapritomi@gmail.com>
2026-02-26 19:04:09 +0100

    pair update

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { useMemo, useState } from 'react'
 
   type Card = { id: number; answer: string; img: string | null };
   type Slot = { id: number; question: string; img: string | null};
-  
   type Placement = Record<number, number | null>;
-
 
   const taskData = {
     task_title: "Magnam.",
@@ -106,17 +104,18 @@ import { useMemo, useState } from 'react'
     answer: a.answer,
     img: a.img,
   }));
+ 
   const initialPlacement: Placement = Object.fromEntries(
   cards.map((c) => [c.id, null])
 );
-
+  
 function App() {
   //Kártyák helyzete, null = poolban van
-   const [placement, setPlacement] = useState<Placement>(initialPlacement);
-
+  const [placement, setPlacement] = useState<Placement>(initialPlacement);
+  //minden hibátlan üzenet
+  const [success, setSuccess] = useState(false);
   //Épp melyik kártyát húzod
   const [draggingId, setDraggingId] = useState<number | null>(null);
-
 
   const cardById = useMemo(() => new Map(cards.map((c) => [c.id, c])), []);
 
@@ -150,102 +149,121 @@ function App() {
   const [checked, setChecked] = useState(false);
   const cardMap = useMemo(() => new Map(cards.map((c) => [c.id, c])), [cards]);
   const slotMap = useMemo(() => new Map(slots.map((s) => [s.id, s])), [slots]);
-
-
   const isCardCorrect = (cardId: number) => {
-  const slotId = placement[cardId];
-  if (!slotId) return false;
-  return cardMap.get(cardId)?.id === slotMap.get(slotId)?.id;
+    const slotId = placement[cardId];
+    if (!slotId) return false;
+    return cardMap.get(cardId)?.id === slotMap.get(slotId)?.id;
+  };
+  //Ellenőrzés hogy minden jó
+  const checkAllCorrect = () => {
+  const allCorrect = cards.every(card =>
+    isCardCorrect(card.id)
+  );
+
+  setSuccess(allCorrect);
+  setChecked(true); // ha használod a keretezéshez
 };
 
   return (
-    <div className='bg-gradient-to-r from-[#E8F7EC] to-[#F0F9FF] h-full flex flex-wrap '>
-      {/* felső rész: slotok */}
-      <div className='grid grid-cols-2 relative w-3/5 gap-y-4 gap-x-12 pt-16 pl-16 pr-12'>
+    <div className='bg-gradient-to-r from-[#E8F7EC] to-[#F0F9FF] h-full'>
+      {/* feladat címe */}
+      <div className='pt-4 pl-16 font-semibold text-[30px]'>{taskData.task_title}</div>
+      {/* feladat leírása */}
+      <div className='pt-4 pl-16 font-semibold text-[20px]'>{taskData.task_description}</div>
+      
+      <div className='flex flex-wrap'>
+        {/* bal rész: slotok */}
+        <div className='grid grid-cols-2 relative w-3/5 gap-y-4 gap-x-8 pt-6 pl-16 pr-12'>
+        
+        {/* fehér választóvonal a 2 rész közé*/}
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 h-[85%] w-[5px] bg-white" />
+          
+          {slots.map((slot) => {
+            const idInSlot = slotCardId(slot.id);
+            const card = idInSlot ? cardById.get(Number(idInSlot)) : null;
 
-       <div className="absolute right-0 top-1/2 -translate-y-1/2 h-[85%] w-[5px] bg-white" />
-        {slots.map((slot) => {
-          const idInSlot = slotCardId(slot.id);
-          const card = idInSlot ? cardById.get(Number(idInSlot)) : null;
-
-          return (
-            <div className='bg-white flex w-full h-[200px] rounded-[8px] p-[5px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
-              key={slot.id}
-              onDragOver={(e) => e.preventDefault()}
-              onDrop={() => dropToSlot(slot.id)}
-              
-            >
-              <div className='w-1/2' style={{ fontWeight: 700 }}>{slot.question}</div>
-
-              {card ? (
+            return (
+              <div className='bg-white flex w-full h-[200px] rounded-[8px] p-[5px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+                key={slot.id}
+                onDragOver={(e) => e.preventDefault()}
+                onDrop={() => dropToSlot(slot.id)}
                 
-                <div className='w-1/2 rounded-[5px]'
+              >
+                <div className='w-1/2' style={{ fontWeight: 700 }}>{slot.question}</div>
+
+                {card ? (
+                  
+                  <div className='w-1/2 rounded-[5px]'
+                    draggable
+                    onDragStart={() => setDraggingId(card.id)}
+                    onDragEnd={() => setDraggingId(null)}
+                    style={{
+                      background: "white",
+                      border: !card
+                      ? "2px dashed gray"
+                      : !checked
+                      ? "2px solid transparent"
+                      : isCardCorrect(card.id)
+                      ? "2px solid green"
+                      : "2px solid red",
+                      boxShadow: "0 12px 30px rgba(0,0,0,0.08)",
+                      cursor: "grab",
+                      userSelect: "none",
+                    }}
+                  >
+                    {card.answer}
+                  </div>
+                ) : (
+                  <div className='bg-[#EDEDED] w-[250px] rounded-[5px] shadow-[inset_0_2.21px_8px_rgba(0,0,0,0.25)]'></div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+          
+        {/* jobb rész: nagy összesített mező (pool) */}
+        <div className='pt-6 w-2/5'
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={dropToPool}
+        >
+          {poolCards.length === 0 ? (
+            <div style={{ opacity: 0.5 }}>Nincs több kártya</div>
+          ) : (
+            //berakott kártya
+            <div className='flex flex-wrap gap-8 px-10 justify-center'>
+              {poolCards.map((c) => (
+                <div
+                  className='bg-white flex w-[250px] h-[190px] rounded-[8px] p-[10px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+                  key={c.id}
                   draggable
-                  onDragStart={() => setDraggingId(card.id)}
+                  onDragStart={() => setDraggingId(c.id)}
                   onDragEnd={() => setDraggingId(null)}
                   style={{
-                    background: "white",
-                    border: !card
-                    ? "2px dashed gray"
-                    : !checked
-                    ? "2px solid transparent"
-                    : isCardCorrect(card.id)
-                    ? "2px solid green"
-                    : "2px solid red",
-                    boxShadow: "0 12px 30px rgba(0,0,0,0.08)",
                     cursor: "grab",
                     userSelect: "none",
                   }}
                 >
-                  {card.answer}
+                  {c.answer}
                 </div>
-              ) : (
-                <div className='bg-[#EDEDED] w-[250px] rounded-[5px] shadow-[inset_0_2.21px_8px_rgba(0,0,0,0.25)]'></div>
-              )}
+              ))}
             </div>
-          );
-        })}
-      </div>
-        
-      {/* alsó rész: nagy összesített mező (pool) */}
-      <div className='pt-[60px] w-2/5'
-        onDragOver={(e) => e.preventDefault()}
-        onDrop={dropToPool}
-      >
-        {poolCards.length === 0 ? (
-          <div style={{ opacity: 0.5 }}>Nincs több kártya</div>
-        ) : (
-          //berakott kártya
-          <div className='flex flex-wrap gap-16 px-10 justify-center'>
-            {poolCards.map((c) => (
-              <div
-                className='bg-white flex w-[250px] h-[190px] rounded-[8px] p-[10px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
-                key={c.id}
-                draggable
-                onDragStart={() => setDraggingId(c.id)}
-                onDragEnd={() => setDraggingId(null)}
-                style={{
-                  cursor: "grab",
-                  userSelect: "none",
-                }}
-              >
-                {c.answer}
-              </div>
-            ))}
+          )}
+        {success && (
+          <div className="mt-4 text-green-600 font-bold text-xl">
+            ✅ Siker! Minden válasz helyes!
           </div>
         )}
 
-
-        <div>
-        <button 
-          className='fixed bottom-4 right-[10px] bg-[#2E6544] text-white font-bold rounded-[8px] px-4 py-2 shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
-          onClick={() => setChecked(true)}>
-            Ellenőrzés
-        </button>
+          <div>
+          <button 
+            className='fixed bottom-4 right-[10px] bg-[#2E6544] text-white font-bold rounded-[8px] px-4 py-2 shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+            onClick={() => {setChecked(true); checkAllCorrect()}}>
+              Ellenőrzés
+          </button>
+        </div>
+          
+        </div>
       </div>
-        
-      </div>
-
       
     </div>
   )
